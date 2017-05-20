40f846e504682d23a6f945be92af643f3b2c5f59
Ryan Morr <rm.morr@gmail.com>
2017-05-19 21:14:53 -0400

    Update to use PhantomJS

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,7 @@ import uglify from 'gulp-uglify';
 import header from 'gulp-header';
 import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
-import mocha from 'gulp-mocha';
+import mocha from 'gulp-mocha-phantomjs';
 import browserify from 'browserify';
 import babelify from 'babelify';
 import del from 'del';
@@ -15,54 +15,73 @@ import buffer from 'vinyl-buffer';
 const banner = '/*! ${pkg.name} v${pkg.version} | ${pkg.homepage} */\n';
 
 const config = {
-    name: 'write',
-    files: './src/**/*.js',
-    entryFile: './src/write.js',
-    outputFile: 'write.js',
-    outputDir: './dist/',
-    specs: './test/*.js'
+    src: {
+        files: './src/**/*.js',
+        entryFile: './src/write.js',
+        outputFile: 'write.js',
+        outputDir: './dist/'
+    },
+    test: {
+        runner: './test/runner.html',
+        specs: './test/specs/*.js',
+        entryFile: './test/specs/write.js',
+        outputDir: './test/',
+        outputFile: 'tests.js'
+    }
 };
 
 gulp.task('clean', () => {
-    return del.sync([config.outputDir]);
+    return del.sync([config.src.outputDir]);
+});
+
+gulp.task('clean:test', () => {
+    return del.sync([config.test.outputDir + config.test.outputFile]);
 });
 
 gulp.task('build', ['clean'], () => {
-    return browserify(config.entryFile, {debug: true, standalone: config.name})
+    return browserify(config.src.entryFile, {debug: true, standalone: pkg.name})
         .transform(babelify)
         .bundle()
-        .pipe(source(config.outputFile))
+        .pipe(source(config.src.outputFile))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(header(banner, {pkg}))
-        .pipe(gulp.dest(config.outputDir))
-        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(config.src.outputDir))
         .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(header(banner, {pkg}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(config.outputDir));
+        .pipe(gulp.dest(config.src.outputDir));
+});
+
+gulp.task('build:test', ['clean:test'], () => {
+    return browserify(config.test.entryFile)
+        .transform(babelify)
+        .bundle()
+        .pipe(source(config.test.outputFile))
+        .pipe(buffer())
+        .pipe(gulp.dest(config.test.outputDir));
 });
 
 gulp.task('lint', () => {
-    return gulp.src(['./gulpfile.babel.js', config.files, config.specs])
+    return gulp.src(['./gulpfile.babel.js', config.src.files, config.test.specs])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('test', () => {
-    return gulp.src(config.specs)
-        .pipe(mocha({
-            ui: 'bdd',
-            reporter: 'spec',
-            compilers: [
-                'babel-core/register'
-            ]
-        }));
+gulp.task('test', ['build:test'], () => {
+    return gulp.src(config.test.runner)
+        .pipe(mocha());
+});
+
+gulp.task('watch', () => {
+    gulp.watch(['./gulpfile.babel.js', config.src.files, config.test.specs], ['lint', 'test']);
 });
 
 gulp.task('default', [
     'lint',
     'test',
-    'build'
+    'build',
+    'watch'
 ]);
