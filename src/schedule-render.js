3c7b06ff03cfcd1c2a4a8efe5cfee7108ad90cc6
Ryan Morr <rm.morr@gmail.com>
2021-12-03 12:20:32 -0500

    Remove fps function and optimize for 60 fps by default

--- src/schedule-render.js
+++ src/schedule-render.js
@@ -1,44 +1,46 @@
 let frame;
 let start;
-let budget = null;
-const batch = [];
+const tasks = [];
+const THRESHOLD = 5;
 
-// Browser's require around 6ms to render a frame
-// https://developers.google.com/web/fundamentals/performance/rail#animation
-const BROWSER_RENDER_TIME = 6;
-
-function render() {
-    frame = null;
-    if (budget == null) {
-        while (batch.length > 0) batch.shift().render();
-    } else {
-        start = performance.now();
-        do {
-            batch.shift().render();
-        } while (batch.length > 0 && ((performance.now() - start) < budget));
-        if (batch.length > 0) {
-            frame = requestAnimationFrame(render);
-        }
-    }
+function getTime() {
+    return performance.now();
 }
 
-export function fps(value) {
-    budget = (typeof value === 'number') ? (1000 / value) - BROWSER_RENDER_TIME : null;
+function hasTasks() {
+    return tasks.length > 0;
+}
+
+function shouldRender() {
+    return hasTasks() && (getTime() - start) < THRESHOLD;
+}
+
+function flush() {
+    frame = null;
+    start = getTime();
+    do {
+        tasks.shift().render();
+    } while (shouldRender());
+    if (hasTasks()) {
+        frame = requestAnimationFrame(flush);
+    }
 }
 
 export function clear() {
     if (frame) {
         cancelAnimationFrame(frame);
     }
-    while (batch.length > 0) batch.shift().cancel();
+    while (hasTasks()) {
+        tasks.shift().cancel();
+    }
 }
 
 export function scheduleRender(callback) {
     return new Promise((resolve, reject) => {
         if (!frame) {
-            frame = requestAnimationFrame(render);
+            frame = requestAnimationFrame(flush);
         }
-        batch.push({
+        tasks.push({
             render: () => resolve(callback()),
             cancel: reject
         });
