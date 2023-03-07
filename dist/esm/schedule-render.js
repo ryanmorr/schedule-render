3aab812ce08e997818e892d29822552322922370
Ryan Morr <rm.morr@gmail.com>
2023-03-07 14:24:14 -0500

    Build v3.0.2

---
+++ dist/esm/schedule-render.js
@@ -0,0 +1,2 @@
+/*! @ryanmorr/schedule-render v3.0.2 | https://github.com/ryanmorr/schedule-render */
+let n,e;const t=[],r=5;function o(){return performance.now()}function u(){return t.length>0}function i(){e=o();do{u()&&t.shift()()}while(o()-e<r);n=null,u()&&(n=requestAnimationFrame(i))}function a(e){return new Promise((r=>{n||(n=requestAnimationFrame(i)),t.push((()=>r(e())))}))}export{a as default};
